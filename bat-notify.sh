d1d4c1bd1840e29f6c19fe61976c28aff14839ec
enigmatic-system <admin@enigmaticsystem.xyz>
2020-02-07 22:55:28 -0800

    Added "sleep 120"
    Added "sleep 120" to else clause because without it the script runs rampant.

--- bat-notify.sh
+++ bat-notify.sh
@@ -8,9 +8,9 @@ BatteryCapacity=`cat /sys/class/power_supply/BAT0/capacity`
 LowLimit=10
 
 	if [ $BatteryCapacity -le $LowLimit ]; then
-		notify-send "Battery low!!!"
+		notify-send "Battery Low!!!"
 		sleep 120
 	else
-		continue
+		sleep 120
 	fi
 done 
